01653c3499390f613da34f5dec6ad8ad03bf2c07
Bulanov Konstantin <leech001@gmail.com>
2018-01-14 14:18:59 +0300

    Add pomp control

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
-#Комнатный термостат или терморегулятор на Orange PI
-Проект комнатного термостата или терморегулятора (контроль температуры) на Oprange PI с использованием управления через Telegram.
+# Комнатный термостат или терморегулятор на Orange PI
+Проект комнатного термостата или терморегулятора (контроль температуры) с управлением насосами на Oprange PI с использованием управления через Telegram.
 
-##Цели
+## Цели
 1. Автоматизация работы газового котла взагородном доме;
 2. Удаленный котроль за состоянием отопления;
 3. Удаленное управление режимом отопления.
+4. Управление насосами отопления.
 
-##Задачи
-1. Выбор аппаратного решения
-2. Выбор программного решения
 
-##Решения
-1. Аппаратные
+# Room thermostat or the thermostat on the Orange PI
+Project room thermostat or the thermostat (temperature control) with pump control at Oprange with the use of PI control via Telegram.
+
+## Goals
+1. Automation of gas boiler zagorodnom house;
+2. Remote monitor the state of heating;
+3. Remote control of heating.
+4. Pump control heating.
+
+## Решения (Solution)
+1. Аппаратные (Hardware)
    1. Orange PI (https://ru.aliexpress.com/item/Orange-Pi-PC-Plus-SET2-Orange-Pi-PC-Plus-Transparent-ABS-Case-Supported-Android-Ubuntu-Debian/32668333407.html)
    2. DALLAS DS18B20 (https://ru.aliexpress.com/item/5pcs-DALLAS-DS18B20-18B20-18S20-TO-92-IC-CHIP-Thermometer-Temperature-Sensor/32236763433.html)
-   3. One Channel Relay Module interface Board Shield 5V (https://ru.aliexpress.com/item/Free-Shipping-1PCS-5V-low-level-trigger-One-1-Channel-Relay-Module-interface-Board-Shield-For/32480128984.html)
-2. Программные решения
+   3. Four Channel Relay Module interface Board Shield 5V (https://ru.aliexpress.com/item/1pcs-lot-4-channel-relay-module-4-channel-relay-control-board-with-optocoupler-Relay-Output-4/32340914033.html?spm=a2g0s.9042311.0.0.8Ihmg6)
+2. Программные решения (Software)
    1. Python 3.5
    2. Telepot
    3. gpio_pyH3
    
-##Активация 1-Wire интерфейса в Orange PI
+## Активация 1-Wire интерфейса в Orange PI
 ```
 root@orangepipcplus:/etc# cat modules
 
@@ -40,7 +47,7 @@ w1-therm
 
 ![GPIO](https://cdn-images-1.medium.com/max/800/1*pcfeGQr_mUJrXDFDrdKMww.png)
 
-##Установка Python library on controlling GPIO pins, I2C and SPI buses 
+## Установка Python library on controlling GPIO pins, I2C and SPI buses
 
 https://github.com/duxingkei33/orangepi_PC_gpio_pyH3
 
@@ -48,7 +55,7 @@ https://github.com/duxingkei33/orangepi_PC_gpio_pyH3
 
 https://github.com/nickoala/telepot
 
-##Аналогичные проекты
+## Аналогичные проекты
 
 http://home-smart-home.ru/telegram-bot-raspberry-pi-signalizaciiya-control/
 
--- temp.py
+++ temp.py
@@ -1,42 +1,47 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import os
-import time
 import datetime
+import os
+import sqlite3
+import sys
+import time
+
 import telepot
+from apscheduler.schedulers.blocking import BlockingScheduler
 from pyA20.gpio import gpio
 from pyA20.gpio import port
-from apscheduler.schedulers.blocking import BlockingScheduler
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, \
+    ForceReply, InlineKeyboardButton, InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent
 
-import sqlite3
-conn = sqlite3.connect('temp.sqlite', check_same_thread=False)
+conn = sqlite3.connect('/usr/local/src/tempcontrol/temp.sqlite', check_same_thread=False)
 db = conn.cursor()
 
+rele = port.PA1
+pomp_1 = port.PA0
+pomp_2 = port.PA3
 
-"""
-start - Начало работы
-heater - Режим отопления
-temp_s - Заданная температура
-temp_set - Задать температуру (/temp_set Температура)
-temp - Вывести температуру
-notify - Уведомления
-notify_change - Задать период уведомлений (/notify_change Период в минутах)
-"""
+a_pomp_1 = 1
+a_pomp_2 = 1
 
-rele = port.PA1 #Порт реле комнатного термостата
 gpio.init()
 gpio.setcfg(rele, gpio.OUTPUT)
+gpio.setcfg(pomp_1, gpio.OUTPUT)
+gpio.setcfg(pomp_2, gpio.OUTPUT)
 
-token = '...'  #Токен к Telegram боту
+token = 'TOKEN'  # TempBot #Токен к Telegram боту
 bot = telepot.Bot(token)
 
-hysteresis = 0.5 #Гистерезис температуры комнатного термостата
-temp = 24.0 #Начальная температура комнатного термостата
-heater = True #Режим нагрева
-t_notify = 60 #Интервал уведомлений (мин)
+hysteresis = 0.5  # Гистерезис температуры комнатного термостата
+temp = 24.0  # Начальная температура комнатного термостата
+heater = True  # Режим нагрева
+t_notify = 180  # Интервал уведомлений (мин)
 
-def getTemperature(idW1):
+message_with_inline_keyboard = None
+s_temp = False
+s_notify = False
+
+
+def get_temperature(idW1):
     filepath = '/sys/devices/w1_bus_master1/' + idW1 + '/w1_slave'
     f = open(filepath, 'r')
     data = f.read()
@@ -45,69 +50,172 @@ def getTemperature(idW1):
 
 
 def on_chat_message(msg):
-    global temp
-    global t_notify
+    global s_temp, temp, s_notify, t_notify, pomp_1, pomp_2, a_pomp_1, a_pomp_2
 
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type == 'text':
         command = msg['text']
         if command == '/start' or command == '/start@LeechDacha_bot':
-            keyboard = {'keyboard': [['@Температура','@Заданная температура'], ['@Режим отопления', '@Уведомления']]}
-            bot.sendMessage(chat_id, str('Привет! Я дачный бот. Чем могу быть полезен?'), reply_markup=keyboard)
+            markup = ReplyKeyboardMarkup(keyboard=[
+                [dict(text='Инфо'), dict(text='Управление'), dict(text='Сигнализация')]
+            ], resize_keyboard=True)
+            bot.sendMessage(chat_id, 'Привет! Я дачный бот. Выбери раздел', reply_markup=markup)
 
-        elif command == '/heater' or command == '/heater@LeechDacha_bot' or command == '@Режим отопления':
-            bot.sendMessage(chat_id, str(heater))
+        elif command == 'Главное меню':
+            markup = ReplyKeyboardMarkup(keyboard=[
+                [dict(text='Инфо'), dict(text='Управление'), dict(text='Сигнализация')]
+            ], resize_keyboard=True)
+            bot.sendMessage(chat_id, 'Выбери раздел', reply_markup=markup)
 
-        elif command == '/temp_s' or command == '/temp_s@LeechDacha_bot' or command == '@Заданная температура':
-            bot.sendMessage(chat_id, 'Заданная температура ' + str(temp) + ' C')
+        elif command == u'Инфо':
+            markup = ReplyKeyboardMarkup(keyboard=[
+                [dict(text='Температура'), dict(text='Уст. температура')],
+                [dict(text='Режим нагрева'), dict(text='Главное меню')],
+            ], resize_keyboard=True)
+            bot.sendMessage(chat_id, 'Выбери объект', reply_markup=markup)
 
-        elif command.startswith('/temp_set') or command.startswith('/temp_set@LeechDacha_bot'):
-            temp = command.replace('/temp_set@LeechDacha_bot', '')
-            temp = float(temp.replace('/temp_set', ''))
-            bot.sendMessage(chat_id, 'Температура установлена на '+ str(temp) + ' C')
-
-        elif command == '/temp' or command == '/temp@LeechDacha_bot' or command == '@Температура':
+        elif command == u'Температура':
             bot_temp(chat_id)
 
-        elif command == '/notify' or command == '/notify@LeechDacha_bot' or command == '@Уведомления':
-            bot.sendMessage(chat_id, 'Уведомления каждые ' + str(t_notify) + ' минут')
+        elif command == u'Уст. температура':
+            bot.sendMessage(chat_id, 'Установленная температура %s C' % temp)
 
-        elif command.startswith('/notify_change') or command.startswith('/notify_change@LeechDacha_bot'):
-            t_notify = command.replace('/notify_change@LeechDacha_bot', '')
-            t_notify = int(t_notify.replace('/notify_change', ''))
-            job_notify.reschedule('interval', minutes=t_notify)
-            bot.sendMessage(chat_id, 'Заданные уведомления каждые ' + str(t_notify) + ' минут')
+        elif command == u'Режим нагрева':
+            bot.sendMessage(chat_id, 'Идет нагрев %s' % heater)
+
+        elif command == u'Управление':
+            markup = InlineKeyboardMarkup(inline_keyboard=[[dict(text='Задать температуру', callback_data='temp_set')]])
+            bot.sendMessage(chat_id, 'Выбери объект', reply_markup=markup)
+
+            markup = ReplyKeyboardMarkup(keyboard=[
+                [dict(text='Насос №2 АВТО'), dict(text='Насос №2 ВКЛ'), dict(text='Насос №2 ВЫКЛ')],
+                [dict(text='Насос №1 АВТО'), dict(text='Насос №1 ВКЛ'), dict(text='Насос №1 ВЫКЛ')],
+                [dict(text='Главное меню')],
+            ], resize_keyboard=True)
+            bot.sendMessage(chat_id, 'Выбери объект', reply_markup=markup)
+
+        elif command == u'Насос №1 АВТО':
+            a_pomp_1 = 1
+            bot.sendMessage(chat_id, 'Включен авто режим для насоса №1 ')
+
+        elif command == u'Насос №2 АВТО':
+            a_pomp_2 = 1
+            bot.sendMessage(chat_id, 'Включен авто режим для насоса №2 ')
+
+        elif command == u'Насос №1 ВКЛ':
+            a_pomp_1 = 0
+            gpio.output(pomp_1, 0)
+            bot.sendMessage(chat_id, 'Включен насос №1 ')
+
+        elif command == u'Насос №2 ВКЛ':
+            a_pomp_2 = 0
+            gpio.output(pomp_2, 0)
+            bot.sendMessage(chat_id, 'Включен насос №2 ')
+
+        elif command == u'Насос №1 ВЫКЛ':
+            a_pomp_1 = 0
+            gpio.output(pomp_1, 1)
+            bot.sendMessage(chat_id, 'Выключен насос №1 ')
+
+        elif command == u'Насос №2 ВЫКЛ':
+            a_pomp_2 = 0
+            gpio.output(pomp_2, 1)
+            bot.sendMessage(chat_id, 'Выключен насос №2 ')
+
+        elif command == u'Сигнализация':
+            markup = InlineKeyboardMarkup(
+                inline_keyboard=[[dict(text='Задать период оповещения', callback_data='notify_set')]])
+            bot.sendMessage(chat_id, 'Выбери объект', reply_markup=markup)
+
+        if s_temp:
+            # если происходит установка температуры
+            if command.isdigit():
+                temp = int(command)
+                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Главное меню')]], resize_keyboard=True)
+                bot.sendMessage(chat_id, str("Температура установлена в %s градусов.") % command, reply_markup=markup)
+                s_temp = False
+            else:
+                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Главное меню')]], resize_keyboard=True)
+                bot.sendMessage(chat_id, str(
+                    "%s - это не целое число. При необходимости пройдите настройку заново. Значение не установлено!") % command,
+                                reply_markup=markup)
+                s_temp = False
+
+        if s_notify:
+            # если происходит установка периода оповещения
+            if command.isdigit():
+                job_notify.reschedule('interval', minutes=int(command))
+                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Главное меню')]], resize_keyboard=True)
+                bot.sendMessage(chat_id, str("Период оповещения каждые %s минут.") % command, reply_markup=markup)
+                s_notify = False
+            else:
+                markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Главное меню')]], resize_keyboard=True)
+                bot.sendMessage(chat_id, str(
+                    "%s - это не целое число. При необходимости пройдите настройку заново. Значение не установлено!") % command,
+                                reply_markup=markup)
+                s_notify = False
+
+
+def on_callback_query(msg):
+    global s_temp, s_notify
+    query_id, from_id, data = telepot.glance(msg, flavor='callback_query')
+    if data == 'temp_set':
+        s_temp = True
+        bot.answerCallbackQuery(query_id, text='Установите температуру. Введите целое число в градусах.',
+                                show_alert=True)
+    elif data == 'notify_set':
+        s_notify = True
+        bot.answerCallbackQuery(query_id, text='Установите период оповещения. Введите целое число в минутах.',
+                                show_alert=True)
+
 
 def timed_send():
-    bot_temp(...)  # You ID or ID group
+    bot_temp(111111111)  # DachaGroup # You ID or ID group
+
 
 def bot_temp(chat_id):
-    bot.sendMessage(chat_id, 'Температура выхода котла: ' + str(getTemperature('28-0316b2ec9aff')) + ' C\n'
-                             'Температура входа котла: ' + str(getTemperature('28-0316b306ffff')) + ' C\n'
-                             'Температура в помещении: ' + str(getTemperature('28-0516a16bcbff')) + ' C')
+    bot.sendMessage(chat_id, "Температура выхода котла: %s\n" \
+                             "Температура входа котла: %s\n" \
+                             "Температура в помещении: %s\n" % (
+                        get_temperature('28-0316b2ec9aff'),
+                        get_temperature('28-0316b306ffff'),
+                        get_temperature('28-0516a16bcbff')))
+
 
 def add_temp():
-    db.execute("INSERT INTO temp_control (date,temp, temp_s, heater) VALUES ('%s','%s','%s','%s')" % (datetime.datetime.now(), getTemperature('28-0516a16bcbff'), temp, heater))
+    db.execute("INSERT INTO temp_control (date,temp, temp_s, heater) VALUES ('%s','%s','%s','%s')" % (
+        datetime.datetime.now(), get_temperature('28-0516a16bcbff'), temp, heater))
     conn.commit()
+
 
 def temp_control():
     global heater
-    r_temp = getTemperature('28-0516a16bcbff')
-    if 0 <= r_temp <=100:
-        if r_temp <= (temp-hysteresis):
+    r_temp = get_temperature('28-0516a16bcbff')
+    if 0.0 <= r_temp <= 75:
+        if r_temp <= (temp - hysteresis):
             gpio.output(rele, 0)
+            if a_pomp_1 == 1:
+                gpio.output(pomp_1, 0)
+            if a_pomp_2 == 1:
+                gpio.output(pomp_2, 0)
             heater = True
-        elif r_temp >= (temp+hysteresis):
+        elif r_temp >= (temp + hysteresis):
             gpio.output(rele, 1)
+            if a_pomp_1 == 1:
+                gpio.output(pomp_1, 1)
+            if a_pomp_2 == 1:
+                gpio.output(pomp_2, 1)
             heater = False
         add_temp()
 
-bot.message_loop({'chat': on_chat_message})
-print ('Listening ...')
+
+bot.message_loop({'chat': on_chat_message,
+                  'callback_query': on_callback_query})
+print('Listening ...')
 
 scheduler = BlockingScheduler()
 job_notify = scheduler.add_job(timed_send, 'interval', minutes=t_notify)
-job_temp  = scheduler.add_job(temp_control, 'interval', seconds=5)
+job_temp = scheduler.add_job(temp_control, 'interval', seconds=5)
 scheduler.start()
 
 while 1:
